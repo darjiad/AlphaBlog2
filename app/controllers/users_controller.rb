d198a25218dbbe78a9f1207bd76154f22a098149
Aditi Darji <adudarji2002@gmail.com>
2023-08-07 22:31:56 +0530

    Add user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController <ApplicationController
+    def show
+        @user=User.find(params[:id])
+        @article = @user.articles
+    end 
     def new  
         @user=User.new
+        
+    end
+    def index
+        @users=User.all
     end
     def create 
         @user=User.new(user_params)
@@ -25,10 +33,18 @@ class UsersController <ApplicationController
         end
 
     end
+
+
+
+
+
+
+
     private
     def user_params
         params.require(:user).permit(:username,:email,:password)
     end
+
 
 end
 
