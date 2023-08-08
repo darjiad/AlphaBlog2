2fc137f54b3cb6d004b7c58429af1845e982e3f3
Aditi Darji <adudarji2002@gmail.com>
2023-08-08 18:46:44 +0530

    Association complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController <ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :require_user,only: [:edit, :update]
+    before_action :require_same_user,only: [:edit,:update, :destroy]
     def show
-        @user=User.find(params[:id])
+      
         @article = @user.articles.paginate(page: params[:page], per_page: 2)
     end 
     def new  
@@ -11,40 +14,51 @@ class UsersController <ApplicationController
         @users=User.paginate(page: params[:page], per_page: 2)
     end
     def create 
-        @user=User.new(user_params)
+        @user=User.new(user_params) 
         if @user.save
+            session[:user_id]=@user.id
             flash[:notice]="Welcome to the Alpha Blog #{@user.username},you have successfully signed-up"
             redirect_to articles_path
         else
             render 'new'
-        end
+        end 
 
     end
     def edit 
-        @user=User.find(params[:id])
+        
     end
     def update
-        @user=User.find(params[:id])
+      
         if @user.update(user_params)
             flash[:notice]="Your account information Successfully updated"
             redirect_to @user
-        else
+        else 
             render 'edit'
         end
-
     end
 
 
-
-
-
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if @user=current_user
+        flash[:notice] = "Account and all associated articles are deleted successfully!"
+        redirect_to articles_path
+    end
 
 
     private
     def user_params
         params.require(:user).permit(:username,:email,:password)
     end
+    def set_user
+        @user=User.find(params[:id])
 
-
-end
+    end
+    def require_same_user
+        if current_user!=@user && !current_user.admin?
+            flash[:alert]="You can only edit and delete your own account" 
+            redirect_to @user 
+       end 
+    end 
+end 
 
